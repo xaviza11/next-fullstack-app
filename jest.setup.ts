b10ae39f595eaec2c16197ba5c16feb4f1ea6439
tryconnection <xaviza11@gmail.com>
2024-03-23 12:32:54 +0100

    delete tests now there is another project whit it

--- jest.setup.ts
+++
@@ -1,8 +0,0 @@
-setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
-
-import fetchMock from 'jest-fetch-mock';
-import { TextEncoder, TextDecoder } from 'util';
-
-Object.assign(global, { TextDecoder, TextEncoder });
-
-fetchMock.enableMocks();
